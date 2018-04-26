ac5fd5d5c35809dc04d0bd065bc14c31eda58acd
gitnew2018 <37026885+gitnew2018@users.noreply.github.com>
2018-04-26 17:51:09 +0200

    Add files via upload

--- periscopeDownloader.js
+++ periscopeDownloader.js
@@ -4,6 +4,7 @@ const fs = require('fs'),
     https = require('https');
 
 var link = process.argv[2];
+
 if (link !== undefined) {
     periscope_vod_downloader(link);
 } else {
@@ -14,19 +15,34 @@ function periscope_vod_downloader(g_m3u_url) {
     var g_TEMP = '/temp/',
         g_DOWNLOAD_DIR = './',
         g_fileName = 'outputFile',
-        g_simultaneous_down = 50, //number of chunks to download at once.
+        g_simultaneous_down = 20, //number of chunks to download at once.
         g_allChunks = [],
         g_allChunksToDownload = [],
         g_readyToAppend = [],
-        g_retry = 50, //on errors, retry this many times
+        g_retry = 70, //on errors, retry this many times
         g_timeToRetry = 20,
         g_retrying = false,
-        g_counter = 0;
+        g_counter = 0,
+        g_cookies = '';
 
-    getM3u(g_m3u_url);
+    get_M3u(g_m3u_url);
 
-    function getM3u(m3u_url) {
-        https.get(m3u_url, function (res) {
+    function request_options(justurl) {
+        var options = {
+            hostname: url.parse(justurl).hostname,
+            path: url.parse(justurl).path,
+        }
+        g_cookies ? options.headers = {
+            'cookie': g_cookies,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.103 Safari/537.36'// It makes no differnce if I add it or not.(?)
+        } : ''; //if cookies, add them to request options.
+        return options;
+    }
+
+    function get_M3u(m3u_url) {
+        var options = request_options(m3u_url);
+
+        https.get(options, function (res) {
             var responseParts = [];
             res.setEncoding('utf8');
             res.on('data', function (dataChunk) {
@@ -34,37 +50,50 @@ function periscope_vod_downloader(g_m3u_url) {
             });
             res.on('end', function () {
                 var m3u_response = responseParts.join(''); //complete m3u text
+                // console.log(m3u_response);
                 var availableStreamsURLs = [];
                 if (m3u_response.indexOf('#EXT-X-PLAYLIST-TYPE:VOD') !== -1) {
                     g_allChunks = m3u_response.split('\n').filter(function (line) { //list of video chunks from m3u8 playlist.
                         return /^chunk_.+/gm.test(line);
                     });
-                    outputNameCheck();
-                    mkTemp();
+                    mk_Temp();
                 } else if (m3u_response.indexOf('#EXT-X-STREAM-INF') !== -1) {
                     availableStreamsURLs = m3u_response.split('\n').filter(function (line) { //list of available streams.
                         return /^\/.+/gm.test(line);
                     });
                     var bestQualUrl = url.resolve('https://' + url.parse(m3u_url).host + '/', availableStreamsURLs[availableStreamsURLs.length - 1])
-                    getM3u(bestQualUrl)
-                } else if (m3u_response.indexOf('#EXTM3U') !== -1); //live
-                else console.log(m3u_response);
+                    get_M3u(bestQualUrl)
+                } else if (res.statusCode === 301) { //private vod redirection link
+                    var prvCookies = res.headers['set-cookie'];
+                    g_m3u_url = res.headers['location'];
+                    for (var cookie in prvCookies) {
+                        cookie = (prvCookies[cookie] + "").split(/\s/).shift(); //get first element for each cookie, splited by " ".
+                        g_cookies += cookie;
+                    }
+                    get_M3u(g_m3u_url);
+                    // cos2(g_m3u_url);
+                } else if (m3u_response.indexOf('#EXTM3U') !== -1){ // live
+                    console.log('Live broadcasts are not supported yet.');
+                } else {
+                    console.log(m3u_response);
+                }
             });
         }).on('error', function (e) {
             console.error('Error when trying to get m3u file. \n', e);
         });
     }
 
-    function mkTemp() {
+    function mk_Temp() {
         fs.mkdir(g_DOWNLOAD_DIR + g_TEMP, function (err) {
             if (err) {
-                if (err.code == 'EEXIST') downloaded_chechker(); // ignore the error if the folder already exists
+                if (err.code == 'EEXIST') existing_chunks_checker(); // ignore the error if the folder already exists
                 else throw err; // something else went wrong
-            } else downloaded_chechker(); // successfully created folder
+            } else existing_chunks_checker(); // successfully created folder
         });
     }
 
     function prepare_to_download(vid_chunks) {
+        g_retrying = false;
         var chunksToDownload = [];
         var chunkUrl = [];
         g_allChunksToDownload = vid_chunks;
@@ -77,13 +106,15 @@ function periscope_vod_downloader(g_m3u_url) {
     }
 
     function download_file_httpsget(file_url, chunk_name, chunksToDownload) {
+        var options = request_options(file_url);
         var file = fs.createWriteStream(g_DOWNLOAD_DIR + g_TEMP + chunk_name);
-        https.get(file_url, function (res) {
+        https.get(options, function (res) {
             if ((res.statusCode !== 200) && (g_retry > 0)) { //video chunk might be incomplete/empty. retry.
                 g_retry -= 1;
+                console.log('statusCode:', res.statusCode);
                 setTimeout(function (file_url, chunk_name, chunksToDownload) {
                     download_file_httpsget.bind(null, file_url, chunk_name, chunksToDownload)
-                }, 2000);
+                }, 1000);
             } else {
                 res.on('data', function (data) {
                     file.write(data);
@@ -97,75 +128,101 @@ function periscope_vod_downloader(g_m3u_url) {
                         g_counter = 0;
                         prepare_to_download(g_allChunksToDownload);
                         if (g_readyToAppend.length >= g_allChunks.length) {
-                            downloaded_chechker();
+                            concat_del_Chunks();
                         }
                     }
                 });
             }
         }).on('error', function (e) {
-            console.error('download file error:', e);
+            console.error('download file error: ', e);
             if (g_retry > 0) {
-                prepare_to_download(g_allChunksToDownload);
+                retry_downloading(e);
             }
         });
     };
 
-    function retryDownloading(e) {
+    function retry_downloading(e) {
         if (!g_retrying) { //when multiple get requests fail, allow only one restart
-            if (e.code === 'ECONNRESET') {//probably reached bandwidth quota, periscope ain't gonna let you download more for some time.
-                g_timeToRetry = 120; //wait 2 mins
-            }else g_timeToRetry = 20;
-            g_retry -= 1;
             g_retrying = true;
-            setTimeout(downloaded_chechker, g_timeToRetry*1000);
+            if (e.code === 'ECONNRESET') { //probably reached bandwidth quota, periscope ain't gonna let you download more for some time.
+                g_timeToRetry = 120; //wait longer.
+            } else g_timeToRetry = 20;
+            g_retry -= 1;
+            setTimeout(prepare_to_download.bind(null, g_allChunksToDownload), g_timeToRetry * 1000);
         }
     }
 
-    function downloaded_chechker() {
+    // when downloading of VOD was somehow interrupted, this will check which video chunks were downloaded to prevent unnecessary redownloading. 
+    function existing_chunks_checker() {
         console.log('Checking files.');
-        var to_download_list = [];
-        var files_checked = 0;
-        g_allChunks.forEach(function (chunk_m3u) {
-            fs.stat(g_DOWNLOAD_DIR + g_TEMP + chunk_m3u, function (err, stats) {
+        var to_download_list = []
+        var filesChecked = 0;
+        var existingFiles = 0;
+        var requestsChecked = 0;
+        g_allChunks.forEach(function (videoChunk) { //checking existance and size of dowloaded video chunks.
+            fs.stat(g_DOWNLOAD_DIR + g_TEMP + videoChunk, function (err, stats) {
                 if (stats) { // chunk downloaded already
-                    stats.size ? '' : to_download_list.push(chunk_m3u); // if file is 0 bytes, add to download list
-                    g_readyToAppend.push(chunk_m3u);
-                    files_checked += 1;
-                    if (files_checked === g_allChunks.length) {
-                        if (to_download_list.length) {
-                            console.log('Checking files complete. ', g_readyToAppend.);
-                            g_retrying = false;
-                            redownload_last_few(to_download_list)
-                        } else {
-                            console.log('Checking files complete.');
-                            console.log('Concatenating.');
-                            concat_del_Chunks();
-                        }
-                    }
-                } else { // download this chunk
-                    files_checked += 1;
-                    to_download_list.push(chunk_m3u);
-                    if (files_checked === g_allChunks.length) {
-                        console.log('Checking files complete');
-                        g_retrying = false;
-                        redownload_last_few(to_download_list);
+                    existingFiles += 1;
+                    filesChecked += 1;
+                    request_file_size(videoChunk, stats.size)
+                } else { // does not exist, download this chunk
+                    filesChecked += 1
+                    to_download_list.push(videoChunk)
+                    if ((filesChecked === g_allChunks.length) && !existingFiles) {
+                        prepare_to_download(to_download_list);
                     }
                 }
             });
-        });
+        })
 
-        function redownload_last_few(to_download_list) {
-            for (var i = 0; i < g_simultaneous_down; i++) {
-                if (g_readyToAppend.length > 0) {
-                    to_download_list.unshift(g_readyToAppend.pop());
+        function request_file_size(videoChunk, fileSize) { //comparing file size on disk to content length from header.
+            var chunkUrl = url.resolve(g_m3u_url, videoChunk);
+            var options = {
+                hostname: url.parse(chunkUrl).hostname,
+                path: url.parse(chunkUrl).path,
+                method: 'HEAD'
+            };
+            g_cookies ? options.headers = {
+                'cookie': g_cookies,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.103 Safari/537.36'
+            } : ''; //if cookies, add them to request options.
+            var req = https.request(options, function (res) {
+                requestsChecked += 1;
+                if (res.headers["content-length"] == fileSize) { // correct filesize, no need to redownload.
+                    g_readyToAppend.push(videoChunk)
+                } else { //                                      // incomplete file, redownload
+                    to_download_list.push(videoChunk)
                 }
-            }
-            prepare_to_download(to_download_list);
+                if ((filesChecked === g_allChunks.length) && (requestsChecked === existingFiles)) {
+                    console.log('verified size of '+requestsChecked + ' file(s), '+ g_readyToAppend.length +' is/are OK.');
+                    if (to_download_list.length) {
+                        prepare_to_download(to_download_list);
+                    } else {
+                        concat_del_Chunks();
+                    }
+                }
+            });
+            req.end();
+            req.on('error', function (e) {
+                console.error('problem with size checking request:', e);
+                requestsChecked += 1; //when problems with checking files online add to download list.
+                to_download_list.push(videoChunk)
+                if ((filesChecked === g_allChunks.length) && (requestsChecked === existingFiles)) {
+                    console.log('verified size of '+requestsChecked + ' file(s), '+ g_readyToAppend.length +' is/are OK.');
+                    if (to_download_list.length) {
+                        prepare_to_download(to_download_list);
+                    } else {
+                        concat_del_Chunks();
+                    }
+                }
+            });
         }
+
     }
 
     function concat_del_Chunks() {
         var i = 0;
+        output_name_check();
         concatRecur(i);
         function concatRecur(i) {
             if (i === g_allChunks.length) { //finished concatenating
@@ -174,7 +231,7 @@ function periscope_vod_downloader(g_m3u_url) {
                         if (err) console.error(err);
                     });
                 });
-                console.log('File saved as: ' + g_fileName + '.ts');
+                console.log('File saved as: ' + 'R_' + g_fileName + '.ts');
             } else {
                 fs.readFile(g_DOWNLOAD_DIR + g_TEMP + g_allChunks[i], {
                     encoding: "binary"
@@ -196,12 +253,12 @@ function periscope_vod_downloader(g_m3u_url) {
         }
     }
 
-    function outputNameCheck(num) { //without it output would just append to previous output file.
+    function output_name_check(num) { //apprend +1 @filename end if duplicate, without it output video would just append to previous output vodeo file.
         var x = num || 0;
         fs.stat(g_DOWNLOAD_DIR + 'R_' + g_fileName + (num ? num : '') + '.ts', function (err, stats) {
             if (stats) {
                 x += 1;
-                outputNameCheck(x);
+                output_name_check(x);
             } else {
                 num ? g_fileName = g_fileName + num : '';
             }
